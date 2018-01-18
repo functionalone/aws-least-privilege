7d115f5975e04689528cab8a675c0d2e38e7af8e
Guy Lichtman <guy@guylichtman.com>
2018-01-18 18:14:34 +0200

    s3 support for global and bucket level operations

--- src/lib/aws-segments/s3.ts
+++ src/lib/aws-segments/s3.ts
@@ -9,11 +9,24 @@ import { logger } from '../logger';
  */
 const OPS_REPLACERS: {[i: string]: string} = {
   SetObjectTagging: "PutObjectTagging", //java uses SetObjectTagging
-  ListObjectsV2: "ListObjects",
+  ListObjectsV2: "ListBucket",
+  ListObjects: "ListBucket",
   CreateMultipartUpload: "PutObject",
   InitiateMultipartUpload: "PutObject",
+  ListBuckets: "ListAllMyBuckets", 
 };
 
+/**
+ * Regex to check for Bucket level operations. Applied after the OPS_REPLACERS
+ * See: https://docs.aws.amazon.com/AmazonS3/latest/dev/using-with-s3-actions.html#using-with-s3-actions-related-to-buckets
+ */
+// tslint:disable-next-line:max-line-length
+const BUCKET_ACTIONS = /^(ListBucketVersions|ListBucketMultipartUploads|ListBucket|GetBucket.+|PutBucket.+|DeleteBucket.+|.+EncryptionConfiguration|.+InventoryConfiguration|.+LifecycleConfiguration|.+MetricsConfiguration|.+ReplicationConfiguration|.+AnalyticsConfiguration|.+AccelerateConfiguration)$/;
+
+/**
+ * Global actions that work on root star
+ */
+const GLOBAL_ACTIONS = /^(ListAllMyBuckets|CreateBucket|DeleteBucket)$/;
 // tslint:disable:jsdoc-format
 /**
  * Parse a segment doc of the form:
@@ -42,19 +55,35 @@ const OPS_REPLACERS: {[i: string]: string} = {
  * @param segmentDoc 
  * @param actionsMap 
  */
-const parseSegment: SegmentParseFunc = function (segmentDoc: any, actionsMap: ResourceActionMap, functionArn: string) {
+const parseSegment: SegmentParseFunc = function(segmentDoc: any, actionsMap: ResourceActionMap, functionArn: string) {
   const aws: any = segmentDoc.aws;
-  if (isEmpty(aws) || !aws.bucket_name) {
-    logger.warn("Couldn't extract S3 bucket info for segment document: ", segmentDoc);
+  if (isEmpty(aws)) {
+    logger.warn("Empty aws section (skipping) for segment: ", segmentDoc);
     return;
   }
-  const arn = `arn:aws:s3:::${aws.bucket_name}/*`;
+  
+  const op = OPS_REPLACERS[aws.operation] || aws.operation;
+  let arn;
+  if(GLOBAL_ACTIONS.test(op)) {
+    arn = 'arn:aws:s3:::*';
+  }
+  else { 
+    if(!aws.bucket_name) {
+      logger.warn("Empty bucket_name for bucket op: [%s] on segment (skipping): ", op, segmentDoc);
+      return;
+    }
+    if(BUCKET_ACTIONS.test(op)) {
+      arn = `arn:aws:s3:::${aws.bucket_name}`;
+    }
+    else {
+      arn = `arn:aws:s3:::${aws.bucket_name}/*`;
+    }
+  }
   let actions = actionsMap.get(arn);
   if (!actions) {
     actions = new Set();
     actionsMap.set(arn, actions);
   }
-  const op = OPS_REPLACERS[aws.operation] || aws.operation;
   actions.add(op);
 };
 
